abffb1f2507235fb7764e9a3402fddeff760ee8e
biggen8990 <mcvey.justin@gmail.com>
2026-05-08 06:51:44 -0500

    Fixed DateTime capitalization

--- app_web.py
+++ app_web.py
@@ -41,7 +41,7 @@ class Appliance(db.Model):
     invoice_file = db.Column(db.String(255))
     last_updated = db.Column(db.String(30), nullable=False, default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     synced = db.Column(db.Boolean, default=False)
-    last_updated = db.Column(db.Datetime, default=lambda: datetime.now(timezone.utc),
+    last_updated = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc),
     onupdate=lambda: datetime.now(timezone.utc)
     )
 class StatusHistory(db.Model):
